be600aff38fe5f3f2880db20e743bfe3df6fc7c4
Ilya Greshnov <greshnoviv@yandex.ru>
2025-02-22 13:55:44 +0300

    Исправить требования 6 ТЗ для автоматической проверки

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,6 @@
-import manager.InMemoryHistoryManager;
-import manager.InMemoryTaskManager;
 import manager.Managers;
 import manager.TaskManager;
 import task.*;
-
-import java.util.ArrayList;
 
 public class Main {
 
