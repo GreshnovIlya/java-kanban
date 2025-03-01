bd62539083c24054578bc945ae07f5d1dc45f7c7
Ilya Greshnov <greshnoviv@yandex.ru>
2025-03-01 15:15:48 +0300

    Выполнить требования 7 ТЗ

---
+++ src/manager/FileBackedTaskManager.java
@@ -0,0 +1,165 @@
+package manager;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import exception.ManagerLoadException;
+import exception.ManagerSaveException;
+import task.Epic;
+import task.Status;
+import task.Subtask;
+import task.Task;
+
+public class FileBackedTaskManager extends InMemoryTaskManager implements TaskManager {
+
+    private File file;
+
+    public FileBackedTaskManager(File file, HistoryManager historyManager) {
+        super(historyManager);
+        this.file = file;
+    }
+
+    public FileBackedTaskManager(HistoryManager historyManager) {
+        super(historyManager);
+    }
+
+    public void save() {
+        Collection<Task> allTask = getAllTask().values();
+        Collection<Epic> allEpic = getAllEpic().values();
+        Collection<Subtask> allSubtask = getAllSubtask().values();
+
+        try (FileWriter fw = new FileWriter(file)) {
+            for (Task task : allTask) {
+                String taskAsString = taskToString(task);
+                fw.write(taskAsString);
+            }
+            for (Epic epic : allEpic) {
+                String epicAsString = epicToString(epic);
+                fw.write(epicAsString);
+            }
+            for (Subtask subtask : allSubtask) {
+                String subtaskAsString = subtaskToString(subtask);
+                fw.write(subtaskAsString);
+            }
+        } catch (IOException exception) {
+            String errorMessage = "Ошибка при сохранении в файл";
+            System.out.println(errorMessage);
+            throw new ManagerSaveException(errorMessage);
+        }
+    }
+
+    private String taskToString(Task task) {
+        return task.getId() + ";TASK;" + task.getName() + ";" + task.getStatus() + ";" + task.getDescription() + ";\n";
+    }
+
+    private String epicToString(Epic epic) {
+        return epic.getId() + ";EPIC;" + epic.getName() + ";" + epic.getStatus() + ";" + epic.getDescription() + ";\n";
+    }
+
+    private String subtaskToString(Subtask subtask) {
+        return subtask.getId() + ";SUBTASK;" + subtask.getName() + ";" + subtask.getStatus() + ";"
+                + subtask.getDescription() + ";" + subtask.getEpicId() + "\n";
+    }
+
+    public static FileBackedTaskManager loadFromFile(File file) {
+        try {
+            int maxCount = 0;
+            FileBackedTaskManager fileBackedTaskManager = new FileBackedTaskManager(file, new InMemoryHistoryManager());
+            List<String> fileToStrings = Files.readAllLines(file.toPath());
+            for (String str : fileToStrings) {
+                String[] value = str.split(";");
+                if (Integer.parseInt(value[0]) > maxCount) {
+                    maxCount = Integer.parseInt(value[0]);
+                }
+                switch(value[1]) {
+                    case "TASK": {
+                        Task task = new Task(Integer.parseInt(value[0]), value[2], value[4], Status.valueOf(value[3]));
+                        fileBackedTaskManager.tasks.put(task.getId(),task);
+                        break;
+                    } case "EPIC": {
+                        Epic epic = new Epic(Integer.parseInt(value[0]), value[2], value[4], Status.valueOf(value[3]),
+                                new ArrayList<>());
+                        fileBackedTaskManager.epics.put(epic.getId(),epic);
+                        break;
+                    } case "SUBTASK": {
+                        Subtask subtask = new Subtask(Integer.parseInt(value[0]), value[2], value[4],
+                                Status.valueOf(value[3]), Integer.parseInt(value[5]));
+                        fileBackedTaskManager.subtasks.put(subtask.getId(), subtask);
+                        Epic epic = fileBackedTaskManager.epics.get(Integer.parseInt(value[5]));
+                        List<Integer> subtasksInEpic = epic.getSubtask();
+                        subtasksInEpic.add(Integer.parseInt(value[0]));
+                        epic.setSubtask(subtasksInEpic);
+                        break;
+                    }
+                }
+                fileBackedTaskManager.count = maxCount + 1;
+            }
+            return fileBackedTaskManager;
+        } catch (IOException exception) {
+            String errorMessage = "Ошибка при загрузки из файла";
+            System.out.println(errorMessage);
+            throw new ManagerLoadException(errorMessage);
+        }
+    }
+
+    @Override
+    public Task createTask(Task task) {
+        Task createTask = super.createTask(task);
+        save();
+        return createTask;
+    }
+
+    @Override
+    public Epic createEpic(Epic epic) {
+        Epic createEpic = super.createEpic(epic);
+        save();
+        return createEpic;
+    }
+
+    @Override
+    public Subtask createSubtask(Subtask subtask) {
+        Subtask createSubtask = super.createSubtask(subtask);
+        save();
+        return createSubtask;
+    }
+
+    @Override
+    public Task updateTask(Task task) {
+        Task createTask = super.updateTask(task);
+        save();
+        return createTask;
+    }
+
+    @Override
+    public Subtask updateSubtask(Subtask subtask) {
+        Subtask updateSubtask = super.updateSubtask(subtask);
+        save();
+        return updateSubtask;
+    }
+
+    @Override
+    public Task deleteTaskById(Integer id) {
+        Task deleteTask = super.deleteTaskById(id);
+        save();
+        return deleteTask;
+    }
+
+    @Override
+    public Epic deleteEpicById(Integer id) {
+        Epic deleteEpic = super.deleteEpicById(id);
+        save();
+        return deleteEpic;
+    }
+
+    @Override
+    public Subtask deleteSubtaskById(Integer id) {
+        Subtask deleteSubtask = super.deleteSubtaskById(id);
+        save();
+        return deleteSubtask;
+    }
+}
---
+++ test/manager/FileBackedTaskManagerTest.java
@@ -0,0 +1,146 @@
+package manager;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import task.Epic;
+import task.Status;
+import task.Subtask;
+import task.Task;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class FileBackedTaskManagerTest {
+    private FileBackedTaskManager taskManager;
+    String name1 = "Сделать";
+    String description1 = "Написать";
+    String name2 = "1";
+    String description2 = "2";
+    String name3 = "11";
+    String description3 = "22";
+    String name4 = "111";
+    String description4 = "222";
+    File check;
+
+    @BeforeEach
+    public void init() throws IOException {
+        check = File.createTempFile("check.csv", null);
+        taskManager = new FileBackedTaskManager(check, new InMemoryHistoryManager());
+    }
+
+    @Test
+    void emptyFile() throws IOException {
+        taskManager.save();
+        taskManager = FileBackedTaskManager.loadFromFile(check);
+        List<String> fileToStrings = Files.readAllLines(check.toPath());
+
+        Assertions.assertNotNull(taskManager);
+        Assertions.assertEquals(fileToStrings.size(), 0);
+    }
+
+    @Test
+    void saveToFile() throws IOException {
+        List<String> checkString = new ArrayList<>();
+        checkString.add("0;TASK;Сделать;NEW;Написать;");
+        checkString.add("1;EPIC;1;NEW;2;");
+        checkString.add("2;SUBTASK;11;NEW;22;1");
+
+        Task task1 = new Task(name1, description1);
+        task1 = taskManager.createTask(task1);
+        Epic epic1 = new Epic(name2, description2);
+        epic1 = taskManager.createEpic(epic1);
+        Subtask subtask1 = new Subtask(name3, description3, epic1.getId());
+        subtask1 = taskManager.createSubtask(subtask1);
+        List<String> fileToStrings = Files.readAllLines(check.toPath());
+
+        Assertions.assertEquals(fileToStrings, checkString);
+    }
+
+    @Test
+    void saveToFileAndLoading() {
+        Map<Integer, Task> checkTask = new HashMap<>();
+        Map<Integer, Task> checkEpic = new HashMap<>();
+        Map<Integer, Task> checkSubtask = new HashMap<>();
+
+        Task task1 = new Task(name1, description1);
+        task1 = taskManager.createTask(task1);
+        checkTask.put(task1.getId(),task1);
+        Epic epic1 = new Epic(name2, description2);
+        epic1 = taskManager.createEpic(epic1);
+        checkEpic.put(epic1.getId(),epic1);
+        Subtask subtask1 = new Subtask(name3, description3, epic1.getId());
+        subtask1 = taskManager.createSubtask(subtask1);
+        checkSubtask.put(subtask1.getId(),subtask1);
+        taskManager = FileBackedTaskManager.loadFromFile(check);
+
+        Assertions.assertEquals(taskManager.tasks.values().toString(), checkTask.values().toString());
+        Assertions.assertEquals(taskManager.epics.values().toString(), checkEpic.values().toString());
+        Assertions.assertEquals(taskManager.subtasks.values().toString(), checkSubtask.values().toString());
+    }
+
+    @Test
+    void saveToFileAndLoadingIfDeleteAndUpdateTask() {
+        Map<Integer, Task> checkTask = new HashMap<>();
+
+        Task task1 = new Task(name1, description1);
+        task1 = taskManager.createTask(task1);
+        Task task2 = new Task(name4, description4);
+        task2 = taskManager.createTask(task2);
+        taskManager = FileBackedTaskManager.loadFromFile(check);
+        taskManager.deleteTaskById(task1.getId());
+        task2 = new Task(task2.getId(), name1, description1, Status.IN_PROGRESS);
+        task2 = taskManager.updateTask(task2);
+        checkTask.put(task2.getId(),task2);
+
+        Assertions.assertEquals(taskManager.tasks.values().toString(), checkTask.values().toString());
+    }
+
+    @Test
+    void saveToFileAndLoadingIfDeleteAndUpdateSubtask() {
+        Map<Integer, Task> checkEpic = new HashMap<>();
+        Map<Integer, Task> checkSubtask = new HashMap<>();
+
+        Epic epic1 = new Epic(name1, description1);
+        epic1 = taskManager.createEpic(epic1);
+        Subtask subtask1 = new Subtask(name2, description2, epic1.getId());
+        subtask1 = taskManager.createSubtask(subtask1);
+        Subtask subtask2 = new Subtask(name3, description3, epic1.getId());
+        subtask2 = taskManager.createSubtask(subtask2);
+        checkSubtask.put(subtask2.getId(),subtask2);
+        Epic epic2 = new Epic(name4, description4);
+        epic2 = taskManager.createEpic(epic2);
+        Subtask subtask3 = new Subtask(name4, description4, epic2.getId());
+        subtask3 = taskManager.createSubtask(subtask3);
+        taskManager = FileBackedTaskManager.loadFromFile(check);
+        taskManager.deleteSubtaskById(subtask1.getId());
+        epic1 = taskManager.epics.get(epic1.getId());
+        subtask3 = new Subtask(subtask3.getId(), name4, description4, Status.IN_PROGRESS, epic2.getId());
+        subtask3 = taskManager.updateSubtask(subtask3);
+        epic2 = taskManager.epics.get(epic2.getId());
+        checkSubtask.put(subtask3.getId(),subtask3);
+        checkEpic.put(epic1.getId(),epic1);
+        checkEpic.put(epic2.getId(),epic2);
+
+        Assertions.assertEquals(taskManager.subtasks.values().toString(), checkSubtask.values().toString());
+        Assertions.assertEquals(taskManager.epics.values().toString(), checkEpic.values().toString());
+    }
+
+    @Test
+    void saveToFileAndLoadingIfDeleteEpic() {
+        Epic epic1 = new Epic(name1, description1);
+        epic1 = taskManager.createEpic(epic1);
+        Subtask subtask1 = new Subtask(name2, description2, epic1.getId());
+        subtask1 = taskManager.createSubtask(subtask1);
+        taskManager = FileBackedTaskManager.loadFromFile(check);
+        taskManager.deleteEpicById(epic1.getId());
+
+        Assertions.assertEquals(taskManager.epics.values().toString(), new HashMap<>().values().toString());
+        Assertions.assertEquals(taskManager.subtasks.values().toString(), new HashMap<>().values().toString());
+    }
+}
